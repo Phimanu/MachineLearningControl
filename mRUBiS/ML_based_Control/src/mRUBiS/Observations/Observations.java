9f0c6070773facc8ac1cea6dc1399d16116fe4c1
Paul Wullenweber <paul.wullenweber@student.uni-tuebingen.de>
2021-05-21 16:47:21 +0200

    Add observation to json changes

--- mRUBiS/ML_based_Control/src/mRUBiS/Observations/Observations.java
+++ mRUBiS/ML_based_Control/src/mRUBiS/Observations/Observations.java
@@ -2,41 +2,103 @@ package mRUBiS.Observations;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.eclipse.emf.ecore.resource.Resource;
 
 import de.mdelab.morisia.comparch.Architecture;
 import de.mdelab.morisia.comparch.Component;
 import de.mdelab.morisia.comparch.Issue;
 import de.mdelab.morisia.comparch.Tenant;
 import de.mdelab.morisia.selfhealing.ArchitectureUtilCal;
+import de.mdelab.morisia.selfhealing.EnvSetUp;
+import de.mdelab.sdm.interpreter.core.SDMException;
 
 public class Observations {
-	public static void makeObservation(Architecture mRUBiS){
+	
+	static Architecture mRubis;
+	
+	public static void main(String[] args) throws SDMException, IOException {
+		Observations.initializeMRubisInstance();
+		Observations.executionLoop();
+	}
+	
+	private static void initializeMRubisInstance() {
+		
+		if (mRubis == null) {
+			Resource architectureResource = EnvSetUp
+					
+					.loadFreshInstance("model/enriched/mRUBiS-1shop_enriched.comparch");
+																								
+																								
+
+			Architecture architecture = (Architecture) architectureResource.getContents().get(0);
+			
+			mRubis = architecture;
+		}
+		
+	}
+	
+	private static void executionLoop() {
+		// inject failures
+	}
+	
+	// TODO:
+	
+	// one static method per python method
+	
+	public static String getComponentsUtility(){
 		
 		
-		ArchitectureUtilCal.computeOverallUtility(mRUBiS);
+		ArchitectureUtilCal.computeOverallUtility(mRubis);
 		
-		for (Tenant shop : mRUBiS.getTenants())
+		for (Tenant shop : mRubis.getTenants())
 		{ArchitectureUtilCal.computeShopUtility(shop);
 		
 			shop.getName();
 			shop.getCriticality();
 			shop.getPerformance();
 			shop.getRequest();
-				
 		
+			HashMap<String, HashMap<String, HashMap<String, Double>>> shopMap = new HashMap<String, HashMap<String, HashMap<String, Double>>>();
+			HashMap<String, HashMap<String, Double>> componentMap = new HashMap<String, HashMap<String, Double>>();
+			shopMap.put(shop.getName(), componentMap);
+			
 		for ( Component component : shop.getComponents())
-		{    ArchitectureUtilCal.computeComponentUtility(component);
-			component.getInUseReplica();
-			component.getIssues();
-			component.getType();
-			component.getCriticality();
+		{    
+			
+			HashMap<String, Double> parameterMap = new HashMap<String, Double>();
+			
+			parameterMap.put("adt", component.getADT());
+			parameterMap.put("connectivity", new Double(component.getProvidedInterfaces().size() + component.getRequiredInterfaces().size()));
+			parameterMap.put("importance", component.getTenant().getImportance());
+			parameterMap.put("reliability", component.getType().getReliability());
+			parameterMap.put("criticality", component.getType().getCriticality());
+			parameterMap.put("request", component.getRequest());
+			parameterMap.put("sat_point", component.getType().getSatPoint());
+			parameterMap.put("replica", component.getInUseReplica());
+			parameterMap.put("perf_max", component.getType().getPerformanceMax());
+			parameterMap.put("component_utility", ArchitectureUtilCal.computeComponentUtility(component));
+			
+			componentMap.put(component.getUid() , parameterMap);
+			
 		}
+		
+		//Gson gson = new Gson(); 
+		//String json = gson.toJson(myObject);
+		
+		// https://stackoverflow.com/questions/12155800/how-to-convert-hashmap-to-json-object-in-java
+		String json = new ObjectMapper().writeValueAsString(shopMap);
+		
+		return json;
+		
 		
 		}
 		
 		
 		List<Issue> allIssues = new LinkedList<>();
-		allIssues.addAll(mRUBiS.getAnnotations().getIssues());
+		allIssues.addAll(mRubis.getAnnotations().getIssues());
 		for (Issue issue : allIssues)
 		{issue.getAffectedComponent();
 		issue.getUtilityDrop();
