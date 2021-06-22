d6a1a80ef91005bc711421bd611df02fc7a7dadb
Paul W <44506902+paul-ww@users.noreply.github.com>
2021-06-22 13:22:39 +0200

    Also return state, health and name per component

--- mRUBiS/ML_based_Control/src/mRUBiS/Observations/Observations.java
+++ mRUBiS/ML_based_Control/src/mRUBiS/Observations/Observations.java
@@ -62,25 +62,28 @@ public class Observations {
 		shop.getPerformance();
 		shop.getRequest();
 
-		HashMap<String, HashMap<String, HashMap<String, Double>>> shopMap = new HashMap<String, HashMap<String, HashMap<String, Double>>>();
-		HashMap<String, HashMap<String, Double>> componentMap = new HashMap<String, HashMap<String, Double>>();
+		HashMap<String, HashMap<String, HashMap<String, String>>> shopMap = new HashMap<String, HashMap<String, HashMap<String, String>>>();
+		HashMap<String, HashMap<String, String>> componentMap = new HashMap<String, HashMap<String, String>>();
 		shopMap.put(shop.getName(), componentMap);
 
 		for ( Component component : shop.getComponents())
 		{    
 
-			HashMap<String, Double> parameterMap = new HashMap<String, Double>();
+			HashMap<String, String> parameterMap = new HashMap<String, String>();
 
-			parameterMap.put("adt", component.getADT());
-			parameterMap.put("connectivity", new Double(component.getProvidedInterfaces().size() + component.getRequiredInterfaces().size()));
-			parameterMap.put("importance", component.getTenant().getImportance());
-			parameterMap.put("reliability", component.getType().getReliability());
-			parameterMap.put("criticality", component.getType().getCriticality());
-			parameterMap.put("request", component.getRequest());
-			parameterMap.put("sat_point", component.getType().getSatPoint());
-			parameterMap.put("replica", component.getInUseReplica());
-			parameterMap.put("perf_max", component.getType().getPerformanceMax());
-			parameterMap.put("component_utility", ArchitectureUtilCal.computeComponentUtility(component));
+			parameterMap.put("name", component.getType().getName());
+			parameterMap.put("state", component.getState().getName());
+			parameterMap.put("health", String.valueOf(component.getHealthStatus()));
+			parameterMap.put("adt", String.valueOf(component.getADT()));
+			parameterMap.put("connectivity", String.valueOf(new Double(component.getProvidedInterfaces().size() + component.getRequiredInterfaces().size())));
+			parameterMap.put("importance", String.valueOf(component.getTenant().getImportance()));
+			parameterMap.put("reliability", String.valueOf(component.getType().getReliability()));
+			parameterMap.put("criticality", String.valueOf(component.getType().getCriticality()));
+			parameterMap.put("request", String.valueOf(component.getRequest()));
+			parameterMap.put("sat_point", String.valueOf(component.getType().getSatPoint()));
+			parameterMap.put("replica", String.valueOf(component.getInUseReplica()));
+			parameterMap.put("perf_max", String.valueOf(component.getType().getPerformanceMax()));
+			parameterMap.put("component_utility", String.valueOf(ArchitectureUtilCal.computeComponentUtility(component)));
 
 			componentMap.put(component.getUid() , parameterMap);
 
